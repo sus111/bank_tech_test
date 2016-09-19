dc3419daea1d91b1f44ce29111bd2679bf81c7e9
sus111 <sgraham535@gmail.com>
2016-09-19 17:30:12 +0100

    add print class

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -7,20 +7,39 @@ describe Transaction do
     allow(Time).to receive(:now).and_return(@time_now)
   end
 
-  describe 'add_transaction' do
+  describe 'deposit' do
     it 'should add a deposit to transaciton with date, credit and balance' do
       transaction = Transaction.new
-      transaction.add(@time_now, 50, 50)
+      transaction.deposit(@time_now, 50, 50)
       expect(transaction.transactions).to eq [{date: @time_now, credit: 50.0,
         debit:"", balance: 50.0}]
     end
 
+    it 'should add a deposit to deposits with date, credit and balance' do
+      transaction = Transaction.new
+      transaction.deposit(@time_now, 50, 50)
+      expect(transaction.deposits).to eq [{date: @time_now, credit: 50.0, balance: 50.0}]
+    end
+
+  end
+
+  describe 'withdrawal' do
+
     it 'should add a withdrawal to transaciton with date, withdrawal and balance' do
       transaction = Transaction.new
-      transaction.add(@time_now, -50, 0)
+      transaction.withdrawal(@time_now, -50, 0)
       expect(transaction.transactions).to eq [{date: @time_now, credit: "",
         debit:50.0, balance: 0.0}]
     end
+
+    it 'should add a withdrawal to withdrawals with date, withdrawal and balance' do
+      transaction = Transaction.new
+      transaction.withdrawal(@time_now, -50, 0)
+      expect(transaction.withdrawals).to eq [{date: @time_now,
+        debit:50.0, balance: 0.0}]
+    end
+
+
   end
 
 end
