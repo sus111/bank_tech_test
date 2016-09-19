625352dab35f5a180081640933b234fb548cea43
sus111 <sgraham535@gmail.com>
2016-09-19 15:56:53 +0100

    2 classes

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -14,15 +14,15 @@ describe Account do
   describe 'add_deposit' do
     it 'adds 50 to the amount' do
       account = Account.new
-      add_deposit.add(50)
-      expect(balance).to eq 50
+      account.add_deposit(50)
+      expect(account.balance).to eq 50
     end
   end
 
   describe 'make_withdrawal' do
     it 'should minus 50 from the account balance' do
       account = Account.new
-      add_deposit.add(50)
+      account.add_deposit(50)
       account.make_withdrawal(50)
       expect(account.balance).to eq 0
     end
